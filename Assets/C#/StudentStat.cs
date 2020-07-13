abc0b835cd550043e39877cb2273f77a22c49008
정수빈 <banyeong321@gmail.com>
2020-07-13 12:55:40 +0900

    training1 commit
    수련일정 변수 선언 및 능력치 증감 적용

--- Assets/C#/StudentStat.cs
+++ Assets/C#/StudentStat.cs
@@ -6,33 +6,21 @@ public class StudentStat : MonoBehaviour
 {
     public static StudentStat instance;
 
-    public int MslStr;//근력
-    public int currentMslStr;
+        public int MslStr;//근력
+        public int currentMslStr = 100;
 
-    public int MoralStr;//도력
-    public int currentMoralStr;
+        public int MoralStr;//도력
+        public int currentMoralStr = 100;
 
-    public int Wealth;//재력
-    public int currentWealth;
+        public int Wealth;//재력
+        public int currentWealth = 100;
 
-    public int Favorability;//호감도
-    public int cureentFavorability;
+        public int Favorability;//호감도
+        public int cureentFavorability = 100;
 
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         instance = this;
-
-        //근력 도력 재력 호감도 50으로 초기화, 초기값은 50임
-        currentMslStr = 50;
-        currentMoralStr = 50;
-        currentWealth = 50;
-        cureentFavorability = 50;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }

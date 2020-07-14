100c36f853714dbd48613af25f8e13e9c49c499a
정수빈 <banyeong321@gmail.com>
2020-07-14 16:05:33 +0900

    Story_1-2
    아직 미완성, 약간의 수정

--- Assets/C#/STORY/story_1.cs
+++ Assets/C#/STORY/story_1.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,26 +14,35 @@ public class Dialogue
 
 public class story_1 : MonoBehaviour //아무런 조건도 충족하지 못함
 {
+    #pragma warning disable 0649
+
     [SerializeField] private SpriteRenderer sprite_Charcter;
     [SerializeField] private SpriteRenderer sprite_dialogue;
-    [SerializeField] private Text txt_dialogue;
+    [SerializeField] private TextMeshPro txt_dialogue;
 
     private bool isDialogue = false;
     private int count = 0;
 
     [SerializeField] private Dialogue[] dialogue;
 
+    public void ShowDialogue() // 대화창 보이게함
+    {
+        sprite_dialogue.gameObject.SetActive(true);
+        sprite_Charcter.gameObject.SetActive(true);
+        txt_dialogue.gameObject.SetActive(true);
+
+        count = 0;
+        isDialogue = true;
+        NextDialogue();
+    }
+
     public void NextDialogue() //대화활성화
     {
         StudentStat studentStat = new StudentStat();
 
-        if ((studentStat.currentMslStr < 300) && (studentStat.currentMoralStr < 300)
-                && (studentStat.currentWealth < 500) && (studentStat.Favorability < 300))
-        {
             txt_dialogue.text = dialogue[count].dialogue;
             sprite_Charcter.sprite = dialogue[count].cg;
             count++;
-        }
     }
 
     // Update is called once per frame
